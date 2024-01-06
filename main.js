7ade5e0004eba88bc72860124427cf8c19637ae9
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-06 19:03:24 +0530

    1

--- main.js
+++ main.js
@@ -6,20 +6,39 @@ const submit = document.querySelector('#submit');
 let count = 0;
 submit.addEventListener('click', () => {
     let val = input.value;
+
+    console.log(val)
     val = val.trim();
+
+    console.log(val)
     val = Number(val);
+
+
     let mult = document.querySelector('#num1').textContent * document.querySelector('#num2').textContent;
+
+
     mult = Number(mult);
+
+
     if( document.querySelector('#input').value === '') {
+
         alert('Please enter a number')
+
     }else if (val === mult) {
+
         console.log("inside if")
         count++;
         score.textContent = count;
+
         num1.textContent = Math.floor(Math.random() * 20);
+
         num2.textContent = Math.floor(Math.random() * 20);
+
     }else {
+
         num1.textContent = Math.floor(Math.random() * 20);
+         
         num2.textContent = Math.floor(Math.random() * 20);
+
     }
 });
